b3ffb5b87b41036530b076f3fd10690b8fc6dd9b
vdevcode <thanhvumaidao1234567@gmail.com>
2024-05-22 12:15:30 +0700

    feat: add components ui thing

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/color-mode", "@vueuse/nuxt", "nuxt-icon"],
+
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/color-mode",
+    "@vueuse/nuxt",
+    "nuxt-icon",
+    "@samk-dev/nuxt-vcalendar",
+    "@vee-validate/nuxt",
+    "@morev/vue-transitions/nuxt"
+  ],
 
   tailwindcss: {
     exposeConfig: true,
@@ -16,16 +25,33 @@ export default defineNuxtConfig({
   },
 
   imports: {
-    imports: [
-      {
-        from: "tailwind-variants",
-        name: "tv",
-      },
-      {
-        from: "tailwind-variants",
-        name: "VariantProps",
-        type: true,
-      },
-    ],
+    imports: [{
+      from: "tailwind-variants",
+      name: "tv",
+    }, {
+      from: "tailwind-variants",
+      name: "VariantProps",
+      type: true,
+    }, {
+      from: "vue-sonner",
+      name: "toast",
+      as: "useSonner"
+    }],
   },
+
+  app: {
+    head: {
+      script: [{
+        src: "https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.9/pdfmake.min.js",
+        defer: true
+      }, {
+        src: "https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.9/vfs_fonts.min.js",
+        defer: true
+      }]
+    }
+  },
+
+  build: {
+    transpile: ["vue-sonner"]
+  }
 });
